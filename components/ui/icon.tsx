b95a96d4fa1ebf4753764e8fcee09d75cd13ff3d
NNNNTTT <arsenal45@icloud.com>
2026-05-24 20:43:16 +0900

    20260524 ダウンロードアイコンのパスを追加
    Icon コンポーネントの PATHS に download を未登録だったため、画像ダウンロードボタンが空で表示されていた問題を修正。

--- components/ui/icon.tsx
+++ components/ui/icon.tsx
@@ -27,6 +27,7 @@ const PATHS: Record<string, string> = {
   arrow_up:         "M12 19V5M5 12l7-7 7 7",
   sun:              "M12 17a5 5 0 1 0 0-10 5 5 0 0 0 0 10zM12 1v2M12 21v2M4.22 4.22l1.42 1.42M18.36 18.36l1.42 1.42M1 12h2M21 12h2M4.22 19.78l1.42-1.42M18.36 5.64l1.42-1.42",
   moon:             "M21 12.79A9 9 0 1 1 11.21 3 7 7 0 0 0 21 12.79z",
+  download:         "M21 15v4a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2v-4M7 10l5 5 5-5M12 15V3",
 }
 
 interface IconProps {
